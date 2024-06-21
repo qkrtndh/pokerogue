ef6b914e595b4c5d899c87a5a9000205fbe06933
qkrtndh <65153512+qkrtndh@users.noreply.github.com>
2024-06-21 17:11:28 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,36 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export const defaultConfig = {
-	plugins: [tsconfigPaths() as any],
-	server: { host: '0.0.0.0', port: 8000 },
-	clearScreen: false,
-	build: {
-		minify: 'esbuild' as const,
-		sourcemap: false,
-	},
-	rollupOptions: {
-		onwarn(warning, warn) {
-			// Suppress "Module level directives cause errors when bundled" warnings
-			if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-				return;
-			}
-			warn(warning);
-		},
-	}
+  plugins: [tsconfigPaths() as any],
+  server: { host: '0.0.0.0', port: 8000 },
+  clearScreen: false,
+  build: {
+    minify: 'esbuild' as const,
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000, // 청크 크기 경고 임계값 설정
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // node_modules 폴더의 모든 라이브러리를 vendor 청크로 분리
+          }
+        },
+      },
+      onwarn(warning, warn) {
+        // Suppress "Module level directives cause errors when bundled" warnings
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
 };
 
-
-export default defineConfig(({mode}) => ({
-	...defaultConfig,
-	esbuild: {
-		pure: mode === 'production' ? [ 'console.log' ] : [],
-		keepNames: true,
-	},
+export default defineConfig(({ mode }) => ({
+  ...defaultConfig,
+  esbuild: {
+    pure: mode === 'production' ? ['console.log'] : [],
+    keepNames: true,
+  },
 }));
